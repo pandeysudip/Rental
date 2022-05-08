6f51e0efd2b57ef9a605ea133d818c0810237531
pandeysudip <sudip@siu.edu>
2022-05-08 12:20:34 -0700

    updating javascript file

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,7 +1,6 @@
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
-
 //function for dropdown menu and initial graphs 
 function init() {
     d3.json('/data/all_data').then((data) => {
@@ -24,30 +23,23 @@ function init() {
         createMap(initSample);
     })
 };
-
 function createMap(commodity) {
     //removing map if already exist
     var container = L.DomUtil.get('map');
     if (container != null) {
         container._leaflet_id = null;
     }
-
     // Define a markerSize() function that will give each city a different radius based on its population.
     function markerSize(value) {
         return Math.sqrt(value) * 1;
     }
-
     d3.json('/data/all_data').then((data) => {
         var cMarkers = []
         for (var i = 0; i < data.length; i++) {
             var d = data[i]
             console.log(d);
-
-
             console.log(d.lat);
             if (d.Commodity == commodity) {
-
-
                 // Setting the marker 
                 cMarkers.push(
                     L.circle([d.lat, d.lng], {
@@ -55,36 +47,29 @@ function createMap(commodity) {
                         fillColor: "green",
                         fillOpacity: 0.75,
                         radius: markerSize(d.rentAmount)
-
                     }).bindPopup(`<h1>${d.houseZip}</h1>  <hr> <h3>Rent:$ ${d.rentAmount.toLocaleString()}</h3>`)
                 )
-
             }
         }
         var zipdata = L.layerGroup(cMarkers);
-
         // Create the tile layer that will be the background of our map.
         var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         });
-
         //google satellite
         googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
             maxZoom: 20,
             subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
         });
-
         // Create a baseMaps object to hold the streetmap layer.
         var baseMaps = {
             "Street Map": streetmap,
             "Google Sat": googleSat
         };
-
         // Create an overlay object to hold our overlay.
         var overlayMaps = {
             Zipcode: zipdata
         };
-
         // Create our map, giving it the streetmap and earthquakes layers to display on load.
         var myMap = L.map("map", {
             center: [
@@ -93,18 +78,13 @@ function createMap(commodity) {
             zoom: 10,
             layers: [streetmap, zipdata]
         });
-
         // Add the layer control to the map.
         L.control.layers(baseMaps, overlayMaps, {
             collapsed: false
         }).addTo(myMap);
-
     })
-
 };
-
 function optionChanged(newSample) {
     createMap(newSample);
 };
-
 init();
