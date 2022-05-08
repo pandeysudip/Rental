d3adfd6ec7d1622acf043922b09f9c02e0d1ef0c
pandeysudip <sudip@siu.edu>
2022-05-07 17:14:02 -0700

    updating app.py

--- app.py
+++ app.py
@@ -10,22 +10,60 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ca_rent"
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', '')
+app.config['MONG_DBNAME'] = 'ca_rent'
 mongo = PyMongo(app)
 
-# Creating collection 
+# Creating collection
 rent = mongo.db['rent']
 
-# Defining route 
+# Defining route
+
+
 @app.route('/')
 def home():
     # Return the template
     return render_template('index.html')
 
+
 @app.route('/index.html')
 def index():
     # Return the template
     return render_template('index.html')
+
+
+@app.route('/dashboard.html')
+def figure():
+    # Store the entire collection as a list
+
+    # Return the template
+    return render_template('dashboard.html')
+
+
+@app.route('/maps.html')
+def plot():
+    # Store the entire collection as a list
+
+    # Return the template
+    return render_template('maps.html')
+
+
+@app.route('/contact.html')
+def contact():
+    # Return the template
+    return render_template('contact.html')
+
+
+@app.route('/data.html')
+def data():
+    # Return the template
+    return render_template('data.html')
+
+
+@ app.route("/data/all_data")
+def get_data():
+    all_data_list = list(rent.find())
+    return json.dumps(all_data_list, default=json_util.default)
 
 
 if __name__ == "__main__":
